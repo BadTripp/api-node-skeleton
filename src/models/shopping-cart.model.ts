7613cc891513a7a6a054a3ab67c4eb63675736cf
RenatoTuretta <renato.turetta00@gmail.com>
2022-08-02 11:24:29 +0200

    Add new logic shoppingcart/patch

--- src/models/shopping-cart.model.ts
+++ src/models/shopping-cart.model.ts
@@ -1,4 +1,5 @@
 export interface ShoppingCart {
     id: string | null;
     userId: string;
+    course : []
 }
--- src/repositories/shopping-cart.repository.ts
+++ src/repositories/shopping-cart.repository.ts
@@ -4,4 +4,9 @@ import { MongoDB } from "../utils/mongodb.utils";
 
 export class ShoppingCartRepository extends MongoDB<ShoppingCart>  {
     protected collection = 'shopping-cart';
+
+    public async getCartByUserId( userId: string ): Promise<ShoppingCart | null> {
+        const result = await this.getDocuments( { userId } );
+        return result?.length > 0 ? result[0] : null;
+      }
 }
